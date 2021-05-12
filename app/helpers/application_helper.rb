ca4c6af63af0bfcbde5e2ffe29d0910270f1aee2
Angelina Moreno <anyelinamoreno@gmail.com>
2021-05-12 15:34:53 +0200

    Add flash messages for feedback to user
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def flash_bootstrap(type)
+    {
+      'error' => :danger,
+      'notice' => :primary,
+      'alert' => :warning,
+      'success' => :success
+    }.fetch(type, 'notice')
+  end
 end
