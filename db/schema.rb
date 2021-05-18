0a13ee3e919de83ae8ce4b5a44252f567e51f6ee
Angelina Moreno <anyelinamoreno@gmail.com>
2021-05-18 17:49:42 +0200

    Add municipalities catalogue
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_153247) do
+ActiveRecord::Schema.define(version: 2021_05_18_154613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "municipalities", force: :cascade do |t|
+    t.string "name"
+    t.bigint "state_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["state_id"], name: "index_municipalities_on_state_id"
+  end
 
   create_table "states", force: :cascade do |t|
     t.string "name"
@@ -41,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_05_18_153247) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "municipalities", "states"
 end
