9e9cc4da3dad2ad83cc822bd7cbee385b36c5eaa
Angelina Moreno <anyelinamoreno@gmail.com>
2021-05-18 18:31:39 +0200

    Add settlement catalogue
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_154613) do
+ActiveRecord::Schema.define(version: 2021_05_18_162726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2021_05_18_154613) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["state_id"], name: "index_municipalities_on_state_id"
+  end
+
+  create_table "settlements", force: :cascade do |t|
+    t.string "postal_code"
+    t.string "name"
+    t.bigint "municipality_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["municipality_id"], name: "index_settlements_on_municipality_id"
   end
 
   create_table "states", force: :cascade do |t|
@@ -50,4 +59,5 @@ ActiveRecord::Schema.define(version: 2021_05_18_154613) do
   end
 
   add_foreign_key "municipalities", "states"
+  add_foreign_key "settlements", "municipalities"
 end
