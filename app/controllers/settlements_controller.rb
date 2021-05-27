a328f6757d7d0ab89eafcb5d6a00812916ba5c5e
Angelina Moreno <anyelinamoreno@gmail.com>
2021-05-27 20:26:06 +0200

    Order alphabetically:
    State, Municipalities and Settlements
    
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>

--- app/controllers/settlements_controller.rb
+++ app/controllers/settlements_controller.rb
@@ -1,6 +1,6 @@
 class SettlementsController < ApiController
   def index
     @municipality = Municipality.find_by(name:params[:municipality])
-    render json: @municipality.settlements
+    render json: @municipality.settlements.order(name: :asc)
   end
 end
