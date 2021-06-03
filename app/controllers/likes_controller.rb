ddc7541f4d43634270fc3ed274f31ae72ccd0e0d
Angelina Moreno <anyelinamoreno@gmail.com>
2021-06-03 14:03:12 +0200

    Implement like unlike using javascript
    Use remote links to handle unobtrusive javascript,
    that allows the controller to respond to Ajax requests
    
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,22 +3,18 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.build(like_params)
-    if @like.save
-      flash[:success] = 'Thanks for liking!'
-    else
-      flash[:alert] = @like.errors.full_messages.join(', ')
+    flash[:alert] = @like.errors.full_messages.join(', ') unless @like.save
+    respond_to do |format|
+      format.js { render 'toggle.js.erb' }
     end
-    redirect_to root_path(anchor: "denounce_#{@like.likeable_id}")
   end
 
   def destroy
     @like = current_user.likes.find(params[:id])
-    if @like.destroy
-      flash[:success] = "Un like #{@like.likeable_type}!"
-    else
-      flash[:alert] = @like.errors.full_messages.join(', ')
+    flash[:alert] = @like.errors.full_messages.join(', ') unless @like.destroy
+    respond_to do |format|
+      format.js { render 'toggle.js.erb' }
     end
-    redirect_to root_path(anchor: "denounce_#{@like.likeable_id}")
   end
 
   protected
---
+++ app/helpers/likes_helper.rb
@@ -0,0 +1,33 @@
+module LikesHelper
+  def likeable?(likeable)
+    current_user.nil? || current_user.like?(likeable).nil?
+  end
+
+  def like_link_for(likeable)
+    options = {
+      method: :post,
+      id: "create-like-#{likeable.id}",
+      class: 'btn btn-link',
+      title: 'like',
+      remote: true,
+      'data-bs-toggle': :tooltip
+    }
+    link_to likes_path(likeable_id: likeable.id, likeable_type: likeable.class.name), options do
+      content_tag(:i, nil, class: 'bi bi-hand-thumbs-up')
+    end
+  end
+
+  def unlike_link_for(likeable)
+    options = {
+      method: :delete,
+      id: "destroy-like-#{likeable.id}",
+      class: 'btn btn-link',
+      title: 'unlike',
+      remote: true,
+      'data-bs-toggle': :tooltip
+    }
+    link_to like_path(current_user.like?(likeable)), options do
+      content_tag(:i, nil, class: 'bi bi-hand-thumbs-up-fill')
+    end
+  end
+end
