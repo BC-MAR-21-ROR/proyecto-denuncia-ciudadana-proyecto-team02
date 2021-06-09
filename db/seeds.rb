860216fd16114ed2fd52abd1d0f789773202c53e
Angelina Moreno <anyelinamoreno@gmail.com>
2021-06-09 16:56:50 +0200

    Add more seeds
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: marianoalarcon <mariano.alarcon@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,42 @@
 # rails db:truncate_all # =>
 # The difference with db:destroy you can execute with server running because delete registre not database
 
-# WE WANT DENOUNCES A
+# USER_TEST
 p "Generating main user test...!"
-user = FactoryBot.create(
-  :user, 
-  username: 'test', 
-  email: 'test@gmail.com', 
-  password: 'test1234', 
+user_test = FactoryBot.create(
+  :user,
+  username: 'test',
+  email: 'test@gmail.com',
+  password: 'test1234',
   password_confirmation: 'test1234'
 )
 
-p "Generating randoms denounces...!"
-denounces = FactoryBot.create_list(:denounce, 10, user: user)
+p "Generating randoms denounces for main user!"
+denounces = FactoryBot.create_list(:denounce, 10, user: user_test)
 
-p "Generating randoms places of interest for #{user.username}...!"
+p "Generating randoms places of interest for #{user_test.username}...!"
 denounces.each do |denounce|
   current_address = denounce.address
   FactoryBot.create(
     :place_of_interest,
-    user: user,
+    user: user_test,
     settlement: current_address.settlement,
     postal_code: current_address.postal_code
   )
 end
+
+
+# GENERATE MORE TEST
+
+def create_seed(number)
+  denounces = FactoryBot.create_list(:denounce, number)
+  denounces.each { |denounce| FactoryBot.create_list(:like, rand(1..10), likeable: denounce) }
+  User.limit(10).each { |user| FactoryBot.create_list(:place_of_interest, rand(1..10), user: user) }
+end
+
+thr = []
+thr << Thread.new { create_seed(25) }
+thr << Thread.new { create_seed(25) }
+thr << Thread.new { create_seed(25) }
+thr << Thread.new { create_seed(25) }
+thr.each(&:join)
