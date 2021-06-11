99d83ca239812f6c832edb499351bdf29aa0a1d6
Angelina Moreno <anyelinamoreno@gmail.com>
2021-06-11 20:34:02 +0200

    Manage diferent number of images in carousel
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: marianoalarcon <mariano.alarcon@hotmail.com>

--- app/helpers/evidence_helper.rb
+++ app/helpers/evidence_helper.rb
@@ -3,9 +3,14 @@ module EvidenceHelper
     images.each_with_index do |image, index|
       concat(
         content_tag(:div, class: "carousel-item #{'active' if index.zero?}") do
-          image_tag(url_for(image), class: 'img-fluid img-thumbnail')
+          if image.image?
+            image_tag(url_for(image), class: 'img-fluid img-thumbnail')
+          else
+            content_tag(:i, nil, class: 'bi bi-card-image')
+          end
         end
       )
     end
+    return
   end
 end
