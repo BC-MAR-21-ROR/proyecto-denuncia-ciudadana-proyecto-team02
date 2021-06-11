48d8f92e61ef46f7d5aa1985e8ad8cee9a401bb1
Angelina Moreno <anyelinamoreno@gmail.com>
2021-06-11 21:06:46 +0200

    Refactor evidence_controller considering rubocop
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: marianoalarcon <mariano.alarcon@hotmail.com>

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -1,21 +1,14 @@
 # frozen_string_literal: true
 
 class EvidencesController < ApplicationController
-
   def destroy
-    begin
-      media = ActiveStorage::Attachment.find(params[:media_id])
-      if media.record.errors.empty?
-        media.purge
-        redirect_back(fallback_location: request.referer)
-      else
-        flash[:error] = "Sorry, image couldn't be upload"
-      end
-    rescue StandardError => e
-      # logger.debug "LOG DE DEGUG rescue: #{@denounce.inspect}"
-      logger.error "LOG DE ERROR rescue #{media.inspect}"
-      logger.error "Mensaje de exception #{e}"
-    end
+    media = ActiveStorage::Attachment.find(params[:media_id])
+    media.purge if media.record.errors.empty?
+  rescue StandardError => e
+    flash[:error] = "Sorry, image couldn't be upload"
+    logger.error "LOG DE ERROR rescue #{media.inspect}"
+    logger.error "Mensaje de exception #{e}"
+  ensure
+    redirect_back(fallback_location: request.referer)
   end
-
 end
