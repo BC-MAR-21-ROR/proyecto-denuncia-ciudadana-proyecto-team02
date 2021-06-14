e57ddccd49755e30f796eeaf296b3bc9ee7efd00
Mariano Alarcon <mariano.alarcon@hotmail.com>
2021-06-14 17:21:31 -0300

    Add logos(pfd and no-image). Add view functionality for attachments in Evidence_helper. - Show: * PDF logo when it is a file of this type with a redirection link, * logo without image when there is no image to display.
    Co-authored-by: Anxhe <anyelinamoreno@gmail.com>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: RobertVel666 <robertvelasco99@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>

--- app/helpers/evidence_helper.rb
+++ app/helpers/evidence_helper.rb
@@ -1,16 +1,43 @@
 module EvidenceHelper
-  def render_evidence_carousel_images(images)
-    images.each_with_index do |image, index|
+
+  def render_evidences(attachments)
+    if attachments.count > 0
+      render_evidence_carousel_preview(attachments)
+    else
+      render_no_image
+    end
+  end
+
+  def render_evidence_carousel_preview(attachments)
+    attachments.each_with_index do |attachment, index|
       concat(
         content_tag(:div, class: "carousel-item #{'active' if index.zero?}") do
-          if image.image?
-            image_tag(url_for(image), class: 'img-fluid img-thumbnail')
+          if attachment.image?
+            image_tag(url_for(attachment), class: 'img-fluid img-thumbnail')
           else
-            content_tag(:i, nil, class: 'bi bi-card-image')
+            link_to(url_for(attachment)) do
+              # byebug
+              image_tag('pdf.svg', class: 'img-fluid img-thumbnail p-5')
+            end
           end
         end
       )
     end
     return
   end
+  
+  def render_no_image
+    image_tag('no-image.svg', class: 'img-fluid img-thumbnail p-5')
+  end
+
+  def render_evidences_edit(attachment)
+    if attachment.image?
+      image_tag(url_for(attachment), class: 'img-size')
+    else
+      link_to(url_for(attachment)) do
+        image_tag('pdf.svg', class: 'img-size')
+      end
+    end
+  end
+
 end
