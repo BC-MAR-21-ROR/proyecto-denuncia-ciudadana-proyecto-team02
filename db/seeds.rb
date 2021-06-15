4ebf8e4e780298291f031038419c4d60d4626077
RobertVel666 <robertvelasco99@gmail.com>
2021-06-15 10:23:34 -0500

    Revert "Merge branch 'master' into Feature#18_Create_more_seeds_to_show_in_app"
      - Reverts for commits: 6767d2744e2db7a9da5103b0e16420877492589e 4d52c405371c6c76c137e1e07de47a6141b5cf2e,  3168f964bef2c43765ca066c370dbcdd0119fad9.
      - Revert to implement: Runs seeds on heroku, fix addressSelector variable Name, add better seeds.
    
    This reverts commit 9c9cbc03b64bc7a78dd634eacdfb6d9fbaec3866, reversing
    changes made to 4d52c405371c6c76c137e1e07de47a6141b5cf2e.
    
    Co-authored-by: Anxhe <anyelinamoreno@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: marianoalarcon <mariano.alarcon@hotmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,75 @@
 # rails db:truncate_all # =>
 # The difference with db:destroy you can execute with server running because delete registre not database
 
-# WE WANT DENOUNCES A
+# USER_TEST
 p "Generating main user test...!"
-user = FactoryBot.create(
-  :user, 
-  username: 'test', 
-  email: 'test@gmail.com', 
-  password: 'test1234', 
+user_test = FactoryBot.create(
+  :user,
+  username: 'test',
+  email: 'test@gmail.com',
+  password: 'test1234',
   password_confirmation: 'test1234'
 )
 
-p "Generating randoms denounces...!"
-denounces = FactoryBot.create_list(:denounce, 10, user: user)
+p "Generating random denounces for main user!"
+denounces = FactoryBot.create_list(:denounce, 10, user: user_test)
 
-p "Generating randoms places of interest for #{user.username}...!"
+p "Generating random places of interest for #{user_test.username}...!"
 denounces.each do |denounce|
   current_address = denounce.address
   FactoryBot.create(
     :place_of_interest,
-    user: user,
+    user: user_test,
     settlement: current_address.settlement,
     postal_code: current_address.postal_code
   )
 end
+
+# GENERATE MORE TEST
+
+def create_users(number)
+  FactoryBot.create_list(:user, number)
+end
+
+def create_denunce(users)
+  users.map do |user|
+    FactoryBot.create_list(:denounce, rand(1..5), user: user)
+  end.flatten
+end
+
+def create_places_of_interest(users)
+  Denounce.all.sample(10).each do |denounce|
+    current_address = denounce.address
+    FactoryBot.create(
+      :place_of_interest,
+      user: users.sample,
+      settlement: current_address.settlement,
+      postal_code: current_address.postal_code
+    )
+  end
+end
+
+def create_likes_random(users)
+  users.each do |user|
+    Denounce.all.sample(10).each do |denounce|
+      FactoryBot.create(:like, likeable: denounce, user: user)
+    end
+  end
+end
+
+def create_seeds(number)
+  print '.'
+  # n = number
+  u = create_users(number)
+  create_denunce(u)
+  create_places_of_interest(u)
+  create_likes_random(u)
+  print '.'
+end
+
+thr = []
+thr << Thread.new { create_seeds(10) }
+thr << Thread.new { create_seeds(10) }
+thr << Thread.new { create_seeds(10) }
+thr << Thread.new { create_seeds(10) }
+thr.each(&:join)
