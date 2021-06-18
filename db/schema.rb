6d10ba6fc0010a11957eb1234527743c32d6b86e
RobertVel666 <robertvelasco99@gmail.com>
2021-06-18 13:51:57 -0500

    Modify event_date from date to datetime
    - Add migration to modify table
    - Change schema version
    
    Co-authored-by: Anxhe <anyelinamoreno@gmail.com>
    Co-authored-by: Arkamis <miguelantonio_rodriguez@ucol.mx>
    Co-authored-by: EusebioAjas <eusebioajas6@gmail.com>
    Co-authored-by: marianoalarcon <mariano.alarcon@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_110053) do
+ActiveRecord::Schema.define(version: 2021_06_18_160346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2021_06_07_110053) do
   end
 
   create_table "denounces", force: :cascade do |t|
-    t.date "event_date"
+    t.datetime "event_date"
     t.integer "area"
     t.string "title"
     t.string "description"
